9d37ff71ccb68831a2fce4c8d557d0c5a27c5ad9
Haythem532002 <khiarih06@gmail.com>
2025-03-06 00:19:37 +0100

    add doctor service and doctor-card component

---
+++ src/app/models/Doctor.ts
@@ -0,0 +1,14 @@
+export interface Doctor {
+  id: number;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  email: string;
+  address: string;
+  city: string;
+  gender: string;
+  experienceYears: number;
+  price: number;
+  title: string;
+  description: string;
+}
---
+++ src/app/services/auth/doctor.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DoctorService {
+  constructor(private http: HttpClient) {}
+  apiUrl = 'http://localhost:7090/doctor';
+  getDoctorByPage(page: number, size: number) {
+    return this.http.get(this.apiUrl + `/all?page=${page}&size=${size}`);
+  }
+}
