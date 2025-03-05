baca22db54fb34b696a2b1f09509232580d6dfe9
Haythem532002 <khiarih06@gmail.com>
2025-03-06 00:18:22 +0100

    working on the doctor list page

--- src/app/pages/doctor/doctor-home/doctor-home.component.ts
+++ src/app/pages/doctor/doctor-home/doctor-home.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-
+import { DoctorCardComponent } from '../../../components/doctor-card/doctor-card.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { DoctorService } from '../../../services/auth/doctor.service';
 @Component({
   selector: 'app-doctor-home',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, DoctorCardComponent, MatPaginatorModule],
   templateUrl: './doctor-home.component.html',
   styleUrl: './doctor-home.component.css',
 })
-export class DoctorHomeComponent {}
+export class DoctorHomeComponent {
+  constructor(private doctorService: DoctorService) {}
+  page = 1;
+  size = 8;
+
+  search() {
+    this.doctorService.getDoctorByPage(this.page, this.size).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  onPageChange(event: any) {
+    this.page = event.pageIndex + 1;
+    this.search();
+  }
+}
